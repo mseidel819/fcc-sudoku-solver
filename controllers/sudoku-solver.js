b2e7b2ac1eb808f48f8fa596c658efc0f203455a
Matt Seidel <mseidel819@gmail.com>
2023-06-19 13:08:37 -0500

    feat: added check route

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,115 @@
-class SudokuSolver {
+// const rowMap = {
+//   a: 0,
+//   b: 1,
+//   c: 2,
+//   d: 3,
+//   e: 4,
+//   f: 5,
+//   g: 6,
+//   h: 7,
+//   i: 8,
+// };
 
+class SudokuSolver {
   validate(puzzleString) {
+    const validChars = /^[1-9.]*$/;
+    if (puzzleString.length !== 81) {
+      return { error: "Expected puzzle to be 81 characters long" };
+    }
+    if (!validChars.test(puzzleString)) {
+      return { error: "Invalid characters in puzzle" };
+    }
+    return true;
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
+    const rowStart = row * 9;
+    const rowEnd = rowStart + 9;
+    const rowValues = puzzleString.slice(rowStart, rowEnd);
 
+    // console.log(rowValues, row);
+    // console.log(!rowValues.includes(value));
+    // console.log(+puzzleString[column - 1 + rowStart] === value);
+    if (value === +puzzleString[column - 1 + rowStart]) {
+      return true;
+    }
+
+    return !rowValues.includes(value);
   }
 
   checkColPlacement(puzzleString, row, column, value) {
+    const colValues = [];
 
+    for (let i = 0; i < 9; i++) {
+      colValues.push(puzzleString[i * 9 + (column - 1)]);
+    }
+
+    if (value === +colValues[row]) {
+      return true;
+    }
+    return !colValues.includes(value.toString());
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
+    const regionRow = Math.floor(row / 3);
+    const regionCol = Math.floor((column - 1) / 3);
+    const regionValues = [];
 
+    //use regionRow and regionCol to push values into regionValues
+    for (let i = 0; i < 3; i++) {
+      const rowStart = (regionRow * 3 + i) * 9 + regionCol * 3;
+      const rowEnd = rowStart + 3;
+
+      regionValues.push(...puzzleString.slice(rowStart, rowEnd));
+    }
+
+    console.log(regionValues, value);
+    console.log(value === +regionValues[(row % 3) * 3 + ((column - 1) % 3)]);
+
+    if (value === +regionValues[(row % 3) * 3 + ((column - 1) % 3)]) {
+      return true;
+    }
+
+    return !regionValues.includes(value.toString());
   }
 
   solve(puzzleString) {
-    
+    //solve the puzzle
+    const puzzle = puzzleString.split("");
+    const solution = puzzleString.split("");
+    const emptyCells = [];
+    for (let i = 0; i < puzzle.length; i++) {
+      if (puzzle[i] === ".") {
+        emptyCells.push(i);
+      }
+    }
+    let index = 0;
+    while (index < emptyCells.length) {
+      const cell = emptyCells[index];
+      const row = Math.floor(cell / 9);
+      const column = cell % 9;
+      const value = solution[cell] === "." ? 1 : parseInt(solution[cell]) + 1;
+      let found = false;
+      while (!found && value < 10) {
+        if (
+          this.checkRowPlacement(puzzleString, row, column, value) &&
+          this.checkColPlacement(puzzleString, row, column, value) &&
+          this.checkRegionPlacement(puzzleString, row, column, value)
+        ) {
+          solution[cell] = value;
+          found = true;
+          index++;
+        } else {
+          value++;
+        }
+      }
+      if (!found) {
+        solution[cell] = ".";
+        index--;
+      }
+    }
+    return solution.join("");
   }
 }
 
 module.exports = SudokuSolver;
-
--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,87 @@
-'use strict';
+"use strict";
 
-const SudokuSolver = require('../controllers/sudoku-solver.js');
+const { parse } = require("dotenv");
+const SudokuSolver = require("../controllers/sudoku-solver.js");
 
 module.exports = function (app) {
-  
   let solver = new SudokuSolver();
 
-  app.route('/api/check')
-    .post((req, res) => {
+  const rowMap = {
+    a: 0,
+    b: 1,
+    c: 2,
+    d: 3,
+    e: 4,
+    f: 5,
+    g: 6,
+    h: 7,
+    i: 8,
+  };
 
-    });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
+  app.route("/api/check").post((req, res) => {
+    // console.log(req.body);
 
-    });
+    if (!req.body.puzzle || !req.body.coordinate || !req.body.value) {
+      return res.json({ error: "Required field(s) missing" });
+    }
+    const letter = req.body.coordinate[0].toLowerCase();
+
+    //get number after letter
+    const number = req.body.coordinate.slice(1);
+    // const number = req.body.coordinate[1];
+
+    const parsedNumber = parseInt(number);
+    const parsedLetter = rowMap[letter];
+    const parsedValue = parseInt(req.body.value);
+
+    //If the coordinate submitted to api/check does not point to an existing grid cell, the returned value will be { error: 'Invalid coordinate'}
+    if (parsedNumber > 9 || parsedNumber < 1 || isNaN(parsedNumber)) {
+      return res.json({ error: "Invalid coordinate" });
+    }
+    if (parsedLetter > 8 || parsedLetter < 0 || isNaN(parsedLetter)) {
+      return res.json({ error: "Invalid coordinate" });
+    }
+
+    //if value is not a number between 1-9, return error
+    if (parsedValue > 9 || parsedValue < 1 || isNaN(parsedValue)) {
+      return res.json({ error: "Invalid value" });
+    }
+
+    const solverParams = [
+      req.body.puzzle,
+      parsedLetter,
+      parsedNumber,
+      parsedValue,
+    ];
+
+    //all of these should accept numbers except fro the puzzle as a whole
+    const isValidRow = solver.checkRowPlacement(...solverParams);
+    const isValidCol = solver.checkColPlacement(...solverParams);
+    const isValidRegion = solver.checkRegionPlacement(...solverParams);
+
+    if (solver.validate(req.body.puzzle).error) {
+      return res.json(solver.validate(req.body.puzzle));
+    }
+
+    if (isValidRow && isValidCol && isValidRegion) {
+      return res.json({ valid: true });
+    }
+    const conflict = [];
+    if (!isValidRow) {
+      conflict.push("row");
+    }
+    if (!isValidCol) {
+      conflict.push("column");
+    }
+    if (!isValidRegion) {
+      conflict.push("region");
+    }
+    return res.json({ valid: false, conflict });
+  });
+
+  app.route("/api/solve").post((req, res) => {
+    const solved = solver.solve(req.body.puzzle);
+    // console.log(req.body.puzzle);
+    // console.log(solved);
+  });
 };
